d283d2e322f167a5d3ea8944dcf4f8133fe7351a
Paulo Montarroios <pauloricardomontarroios@hotmail.com>
2017-10-23 22:49:38 -0200

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/prm/cursomc/services/CategoriaService.java
+++ src/main/java/com/prm/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.prm.cursomc.domain.Categoria;
 import com.prm.cursomc.repositories.CategoriaRepository;
+import com.prm.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -13,8 +14,11 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	
 	public Categoria buscar(Integer id) {
-		Categoria obj = repo.findOne(id);
 		
+		Categoria obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Recurso inexistente !! ");
+		}
 		return obj;
 	}	
 
